b266c8bb7999a48ee23114262d1dfaf9721f002d
artivis <jeremie.deray@canonical.com>
2024-11-25 16:59:15 +0100

    fix launch script

--- snap/local/launch.sh
+++ snap/local/launch.sh
@@ -35,4 +35,4 @@ if [ "${LAUNCH_OPTIONS}" ]; then
   logger -t ${SNAP_NAME} "Running with options: ${LAUNCH_OPTIONS}"
 fi
 
-ros2 launch foxglove_bridge foxglove_bridge_launch.xml ${LAUNCH_OPTIONS}
+roslaunch foxglove_bridge foxglove_bridge.launch ${LAUNCH_OPTIONS}
