8793c78a22f5ccae8cbfdc3392b79e726b668bae
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2025-10-16 11:41:56 +0200

    fix launching

--- snap/local/launch.sh
+++ snap/local/launch.sh
@@ -36,4 +36,4 @@ if [ "${LAUNCH_OPTIONS}" ]; then
   logger -t ${SNAP_NAME} "Running with options: ${LAUNCH_OPTIONS}"
 fi
 
-roslaunch foxglove_bridge foxglove_bridge_launch.xml ${LAUNCH_OPTIONS}
+roslaunch foxglove_bridge foxglove_bridge.launch ${LAUNCH_OPTIONS}
